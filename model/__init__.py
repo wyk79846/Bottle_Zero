cc91f170e817a05ccf63e7e1169578a5bc9e725c
wyk <2956393676@qq.com>
2026-03-04 19:18:21 +0800

    first

---
+++ model/__init__.py
@@ -0,0 +1,19 @@
+from .config import SpongeBobConfig
+from .model_spongebob_pro import (
+    SpongeBobModel,
+    SpongeBobForCausalLM,
+    SpongeBobBlock,
+    Attention,
+    FeedForward,
+    RMSNorm
+)
+
+__all__ = [
+    "SpongeBobConfig",
+    "SpongeBobModel",
+    "SpongeBobForCausalLM",
+    "SpongeBobBlock",
+    "Attention",
+    "FeedForward",
+    "RMSNorm"
+]
